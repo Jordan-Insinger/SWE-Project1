b12c36ddc5f36be2aee974867731f65508d39ba2
bradleyjohnsxn <62025141+bradleyjohnsxn@users.noreply.github.com>
2022-03-23 09:16:54 -0400

    Sort vectors of pointers rather than objects, and pass vectors by reference rather than value.

--- Code/task_sorting.h
+++ Code/task_sorting.h
@@ -6,18 +6,20 @@
 using namespace std;
 
 // Used in sort functions.
-enum attributes { weight, diff, course, date };
+enum class attributes { weight, diff, course, date };
+
+
 
 /*------------------------------------------ Main Sort -----------------------------------------*/
-vector<task> task_sort(vector<task> tasks, attributes parameter, bool ascending= true);
+vector<task*> task_sort(vector<task*> tasks, attributes parameter, bool ascending= true);
 
 /*------------------------------------------ Merge Sort -----------------------------------------*/
 
-vector<task> merge_sort(vector<task> tasks, int left, int right, attributes var);
-vector<task> merge(vector<task> left, vector<task> right, char var);
+vector<task*> merge_sort(vector<task*> tasks, int left, int right, attributes var);
+vector<task*> merge(vector<task*> left, vector<task*> right, char var);
 
 /*------------------------------------------ Helpers -----------------------------------------*/
 
 string lower_string(string s);
 bool before(string l, string r);
-vector<task> reverse(vector<task> v);
+vector<task*> reverse(vector<task*> v);
