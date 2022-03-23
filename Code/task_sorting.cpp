5d4ff1a0dba240129ecf8d6c806c636ebc1bf792
bradleyjohnsxn <62025141+bradleyjohnsxn@users.noreply.github.com>
2022-03-23 16:29:58 -0400

    Changed sorting back

--- Code/task_sorting.cpp
+++ Code/task_sorting.cpp
@@ -1,8 +1,8 @@
 #include "task_sorting.h"
 
 /*------------------------------------------ Main Sort -----------------------------------------*/
-vector<task*> task_sort(vector<task*> tasks, attributes parameter, bool ascending) {
-	vector<task*> result = merge_sort(tasks, 0, tasks.size() - 1, parameter);
+vector<task> task_sort(vector<task> tasks, attributes parameter, bool ascending) {
+	vector<task> result = merge_sort(tasks, 0, tasks.size() - 1, parameter);
 	if (ascending)
 		return result;
 	else
@@ -12,11 +12,11 @@ vector<task*> task_sort(vector<task*> tasks, attributes parameter, bool ascendin
 
 /*------------------------------------------ Merge Sort -----------------------------------------*/
 
-vector<task*> merge_sort(vector<task*> tasks, int left, int right, attributes var) {
+vector<task> merge_sort(vector<task> tasks, int left, int right, attributes var) {
 	if (right > left) {
 		int middle = left + (right - left) / 2;
-		vector<task*> one = merge_sort(tasks, left, middle, var);
-		vector<task*> two = merge_sort(tasks, middle + 1, right, var);
+		vector<task> one = merge_sort(tasks, left, middle, var);
+		vector<task> two = merge_sort(tasks, middle + 1, right, var);
 		switch (var) {
 		case (attributes::weight):
 			return merge(one, two, 'w');
@@ -36,22 +36,22 @@ vector<task*> merge_sort(vector<task*> tasks, int left, int right, attributes va
 		
 	}
 	else {
-		vector<task*> hold;
+		vector<task> hold;
 		hold.push_back(tasks[left]);
 		return hold;
 	}
 }
 
 // Merge vectors in ascending order for merge_sort.
-vector<task*> merge(vector<task*> left, vector<task*> right, char var) {
-	vector<task*> res;
+vector<task> merge(vector<task> left, vector<task> right, char var) {
+	vector<task> res;
 	unsigned int i = 0, j = 0;
 	while (i < left.size() || j < right.size()) {
 		if (i < left.size() && j < right.size()) {
 
 			// Sort by weights.
 			if (var == 'w') {
-				if (left[i]->get_weight() < right[j]->get_weight()) {
+				if (left[i].get_weight() < right[j].get_weight()) {
 					res.push_back(left[i]);
 					i++;
 				}
@@ -63,7 +63,7 @@ vector<task*> merge(vector<task*> left, vector<task*> right, char var) {
 
 			// Sort by difficulty.
 			else if (var == 'd') {
-				if (left[i]->get_diff() < right[j]->get_diff()) {
+				if (left[i].get_diff() < right[j].get_diff()) {
 					res.push_back(left[i]);
 					i++;
 				}
@@ -75,7 +75,7 @@ vector<task*> merge(vector<task*> left, vector<task*> right, char var) {
 
 			// Sort by difficulty.
 			else if (var == 'c') {
-				if (lower_string(left[i]->get_course()) < lower_string(right[j]->get_course())) {
+				if (lower_string(left[i].get_course()) < lower_string(right[j].get_course())) {
 					res.push_back(left[i]);
 					i++;
 				}
@@ -87,7 +87,7 @@ vector<task*> merge(vector<task*> left, vector<task*> right, char var) {
 
 			// Sort by due date.
 			else if (var == 'z') {
-				string l_date = left[i]->get_duedate(), r_date = right[j]->get_duedate();
+				string l_date = left[i].get_duedate(), r_date = right[j].get_duedate();
 
 				if (before(l_date, r_date)) {
 					res.push_back(left[i]);
@@ -155,10 +155,10 @@ bool before(string l_date, string r_date) {
 }
 
 // Reverse a task vector.
-vector<task*> reverse(vector<task*> v) {
+vector<task> reverse(vector<task> v) {
 	int len = v.size();
 	for (unsigned int i = 0; i < v.size()/2; i++) {
-		task* temp = v[len - 1 - i];
+		task temp = v[len - 1 - i];
 		v[len - 1 - i] = v[i];
 		v[i] = temp;
 	}
